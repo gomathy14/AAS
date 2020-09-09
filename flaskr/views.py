8f4e285600d45a541b0d22ff1b8d95dfcd0013d5
igokulganesh <47038733+igokulganesh@users.noreply.github.com>
2020-09-09 13:56:12 +0530

    payment

--- flaskr/views.py
+++ flaskr/views.py
@@ -15,15 +15,11 @@ def index():
     posts = db.execute(
         'SELECT p.id, Name,Block,UnitNo,MaintenanceFee,Due'
         ' FROM income p'
-        
-        
+         
     ).fetchall()
     return render_template('index.html', posts=posts)
 
-
-
 @bp.route('/create', methods=('GET', 'POST'))
-
 def create():
     if request.method == 'POST':
         Name = request.form['Name']
@@ -50,3 +46,31 @@ def create():
             return redirect(url_for('views.index'))
 
     return render_template('create.html')    
+
+
+@bp.route('/payment/<int:income_id>', methods=('GET', 'POST'))
+def payment(income_id):
+    if request.method == 'POST':
+        amount = request.form['amount']
+        error = None
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO Payment (income_id,amount)'
+                ' VALUES (?, ?)',
+                (income_id, amount)
+            )
+            db.commit()
+            return redirect(url_for('views.index'))
+    else: 
+
+        db = get_db()
+        due = db.execute(
+            'SELECT Due'
+            ' FROM income p WHERE p.id == income_id'
+            ).fetchall()
+
+    return render_template('payment.html', due=due)   
